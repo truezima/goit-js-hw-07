cf19693b0f6f4110857426bda3add3fccf0a6261
ValentynaN <sheel@ukr.net>
2026-02-17 20:28:58 +0200

    js-hw-07

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+const categoriesList = document.querySelector('#categories');
+
+const categories = categoriesList.querySelectorAll('.item');
+
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(category => {
+  const title = category.querySelector('h2').textContent;
+  const itemsCount = category.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${itemsCount}`);
+});
